cc8e34625b0401214ed3f3585134487e8d2f34d1
izumi-kadoya <mt.kachikachi.if.cs@gmail.com>
2023-09-04 15:16:04 +0900

    データカラムの修正

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,4 +1,5 @@
 require 'csv'
+
 class RecordsController < ApplicationController
   def index
     @companies = Record.select('company_name, MIN(id) as id').group(:company_name).map { |r| [r.company_name, r.id] }
@@ -15,14 +16,13 @@ class RecordsController < ApplicationController
 
   def create
     uploaded_io = record_params[:csv]
-
     if uploaded_io
       CSV.foreach(uploaded_io.path, headers: true) do |row|
         Record.create(map_row_to_record(row))
       end
       redirect_to records_path, notice: 'CSV was successfully uploaded.'
     else
-      render :new
+      puts "Failed to save record: #{record.errors.full_messages.join(", ")}"
     end
   end
 
@@ -34,45 +34,30 @@ class RecordsController < ApplicationController
 
   def record_params
     params.require(:record).permit(
-      :csv, 
-      :company_name, 
-      :dl_date, 
-      :business_partner, 
-      :company_type, 
-      :country, 
-      :news_snippet, 
-      :url, 
-      :description, 
-      :business_description, 
-      :article_summary, 
-      :text, 
-      :sector, 
-      :industry, 
-      :sub_industry, 
-      :competitors, 
-      :founded_year, 
-      :latest_funding_round, 
-      :latest_funding_date, 
-      :latest_funding_amount, 
-      :latest_funding_simplified_round, 
-      :latest_funding_investors, 
-      :total_funding, 
-      :all_investors, 
-      :link_date, 
-      :link, 
-      :expert_tag, 
-      :date_added, 
-      :added_by, 
-      :date_last_edited, 
-      :last_edited_by, 
-      :company_status, 
-      :exit_date, 
-      :acquirers, 
-      :latest_valuation, 
+      :csv,
+      :company_industry,
+      :company_name,
+      :article_date,
+      :business_partner,
+      :country,
+      :news_snippet,
+      :url,
+      :description,
+      :business_description,
+      :article_summary,
+      :sub_industry,
+      :founded_year,
+      :latest_funding_round,
+      :latest_funding_date,
+      :latest_funding_investors,
+      :total_funding,
+      :all_investors,
+      :exit_date,
+      :acquirers,
+      :latest_valuation,
       :city
     )
   end
-
 
   def from_excel_serial_date(serial_date)
     # Excelのシリアル日付値の基点は1900年1月1日
@@ -80,45 +65,30 @@ class RecordsController < ApplicationController
     adjusted_serial_date = serial_date.to_i - (serial_date.to_i > 59 ? 2 : 1) # Excelの1900年2月29日のバグを考慮
     base_date + adjusted_serial_date.days
   end
+
   def map_row_to_record(row)
     {
+      company_industry: row['company_industry'],
       company_name: row['company_name'],
-      dl_date: from_excel_serial_date(row['dl_date']),
+      article_date: from_excel_serial_date(row['article_date']),
       business_partner: row['business_partner'],
-      company_type: row['company_type'],
       country: row['country'],
       news_snippet: row['news_snippet'],
       url: row['url'],
       description: row['description'],
       business_description: row['business_description'],
       article_summary: row['article_summary'],
-      text: row['text'],
-      sector: row['sector'],
-      industry: row['industry'],
       sub_industry: row['sub_industry'],
-      competitors: row['competitors'],
       founded_year: row['founded_year'].to_i,
       latest_funding_round: row['latest_funding_round'],
       latest_funding_date: from_excel_serial_date(row['latest_funding_date']),
-      latest_funding_amount: row['latest_funding_amount'].to_d,
-      latest_funding_simplified_round: row['latest_funding_simplified_round'],
       latest_funding_investors: row['latest_funding_investors'],
       total_funding: row['total_funding'].to_d,
       all_investors: row['all_investors'],
-      link_date: from_excel_serial_date(row['link_date']),
-      link: row['link'],
-      expert_tag: row['expert_tag'],
-      date_added: from_excel_serial_date(row['date_added']),
-      added_by: row['added_by'],
-      date_last_edited: from_excel_serial_date(row['date_last_edited']),
-      last_edited_by: row['last_edited_by'],
-      company_status: row['company_status'],
       exit_date: from_excel_serial_date(row['exit_date']),
       acquirers: row['acquirers'],
       latest_valuation: row['latest_valuation'].to_d,
       city: row['city']
     }
   end
-  
-
 end
