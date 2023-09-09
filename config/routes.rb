ecf71a6f962644b3027ffd11d238007eabcc0bcc
izumi-kadoya <mt.kachikachi.if.cs@gmail.com>
2023-09-09 15:41:43 +0900

    ルーティングの修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   
   resources :records do
-    resources :comments, only: [:new,:create,:edit,:update]
     
     member do
       get 'partner_details'
