8b2a8ba1c5bc19be6411bcb94e1361e9fb8a4fd2
izumi-kadoya <mt.kachikachi.if.cs@gmail.com>
2023-09-11 13:53:18 +0900

    バリデーションの追加

--- app/models/record.rb
+++ app/models/record.rb
@@ -1,5 +1,5 @@
 class Record < ApplicationRecord
   mount_uploader :csv, CsvUploader
-  validates :company_name, presence: true
+  validates :company_name,:business_partner,:article_date, presence: true
 
 end
