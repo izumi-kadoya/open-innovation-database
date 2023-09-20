da4a5023f1d3890f8c13eab17f9a8ff0523d0fdd
izumi-kadoya <mt.kachikachi.if.cs@gmail.com>
2023-09-20 12:57:26 +0900

    修正

--- app/javascript/speak.js
+++ app/javascript/speak.js
@@ -5,7 +5,7 @@ let isLoaded = false;
 document.getElementById("read-aloud").addEventListener("click", function() {
   if (!isLoaded) { // 初めてのクリック
     var text = document.querySelector(".more-description").innerText;
-    var recordId = this.getAttribute('data-record-id');
+    const recordId = this.getAttribute("record-id");
     fetch(`/records/${recordId}/text_to_speech`, {
       method: 'POST',
       headers: {
